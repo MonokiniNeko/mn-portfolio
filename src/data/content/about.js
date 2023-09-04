0f47b9394069e020dc7bf1139b59659f19fc505c
MonokiniNeko <53049751+MonokiniNeko@users.noreply.github.com>
2023-09-04 13:45:07 +0200

    Create about.js

---
+++ src/data/content/about.js
@@ -0,0 +1,21 @@
+const about = {
+    intro: {
+        en: "I'm a <b>Fullstack JavaScript developer</b> and a Swiss-Army knife of numeric content creation.",
+        fr: 'Je suis un <b>développeur JavaScript Fullstack</b> et un couteau-suisse de la création de contenu numérique.'
+
+    },
+    p1: {
+        en: `The tools and the <b>digital creation</b> always fascinated me, whether it be informatics, the internet, audiovisual or video games. They all have a <b>huge potential</b> that is only growing with the technological advancement.`,
+        fr: `Les outils et la <b>création numérique</b> m'ont toujours fasciné, à savoir l'informatique, l'internet', l'audiovisuel ou encore les jeux vidéo. Ils ont tous un <b>potentiel énorme</b> qui ne fait que croître avec les avancées technologiques.`
+    },
+    p2: {
+        en: `I have been hesitant for a longtime about the path I wanted to follow. Either as a multimedia content creator, a game designer, a copywriter or any other digital vocation. That's why today I'm versatile and have many transferable skills`,
+        fr: `J'ai longtemps été hésitant sur la voie que je voulais suivre, que ce soit en temps que créateur de contenu multimédia, game designer, rédacteur ou autres vocations numérique. C'est pourquoi, aujourd'hui je dispose de beaucoup de cordes à mon arc en lien direct ou non avec le développement.`
+    },
+    p3 : {
+        en : `in light of the above, I consider the <b>Web Development</b> as an outcome of most of my various skills. Put together they can give life to a lot of projects that are as exciting that they are diverse.`,
+        fr : `En définitive, je considère le <b>développement web</b> comme l'aboutissement d'une grande partie de ces diverses compétences acquises afin de les assembler et de donner vie à des projets aussi variés que passionants.`
+    }
+}
+
+export default about
